ac95615be05123a7f15b0533665b914b77ffe01a
Andreas Worth <andreas.worth@gmail.com>
2016-10-21 09:49:40 +0200

    more unit tests

--- src/test/java/com/axa/sea/CellTest.java
+++ src/test/java/com/axa/sea/CellTest.java
@@ -6,8 +6,28 @@ import org.junit.Test;
 public class CellTest {
 
     @Test
-    public void assertThatNewCellsHasColourUnknown() {
+    public void assertThatNewCellsHasUnknownColour() {
         Assert.assertEquals(Cell.TokenColour.UNKNOWN, new Cell().getColour());
+    }
+
+    @Test
+    public void assertThatNewCellsIsEmptyl() {
+        Cell cell = new Cell();
+        Assert.assertTrue(cell.isEmpty());
+    }
+
+    @Test
+    public void assertThatCellsWithUnknownColoureIsEmptyl() {
+        Cell cell = new Cell();
+        cell.setColour(Cell.TokenColour.UNKNOWN);
+        Assert.assertTrue(cell.isEmpty());
+    }
+
+    @Test
+    public void assertThatCellsWithoutUnknownColourIsNotEmpty() {
+        Cell cell = new Cell();
+        cell.setColour(Cell.TokenColour.RED);
+        Assert.assertFalse(cell.isEmpty());
     }
 
 }
